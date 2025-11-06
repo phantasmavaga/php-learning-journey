396f2c5435533e1e27cf565dbd5a4dbeab278bc8
phantasmavaga <phantasmavaga00@proton.me>
2025-11-06 17:58:04 +0300

    age

--- age.php
+++ age.php
@@ -1,18 +1,18 @@
 <?php
-$yas =17;
-if ($yas==15 || $yas==16 ||$yas==17||$yas==18) {
-    echo "Artık genç sayılırsın";
+$age = 17;
+if ($age == 15 || $age == 16 || $age == 17 || $age == 18) {
+    echo "You’re considered a teenager now";
 }
-elseif ($yas>18 && $yas <=25) {
-    echo "Erişkin olma yolundasın";
+elseif ($age > 18 && $age <= 25) {
+    echo "You’re on your way to adulthood";
 }
-elseif ($yas>25 && $yas <=30) {
-    echo "Erişkinsin";
+elseif ($age > 25 && $age <= 30) {
+    echo "You’re an adult";
 }
-elseif ($yas>30) {
-    echo "Yaşlanıyorsun";
+elseif ($age > 30) {
+    echo "You’re getting older";
 }
 else {
-    echo "Daha Çocuksun";
+    echo "You’re still a child";
 }
 ?>
